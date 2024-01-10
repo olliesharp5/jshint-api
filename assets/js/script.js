277f95975dcd924c5ba22f40c8af25b37eee5270
Oliver Sharp <olliesharp5@gmail.com>
2024-01-10 16:18:02 +0000

    Added lesson content

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+const API_KEY = "JzVJeBVnAMHprTHNq62_tHTZC44";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+async function postForm(e) {
+    const form = new FormData(document.getElementById("checksform"));
+
+    const response = fetch("https://ci-jshint.herokuapp.com/api", {
+                        method: "POST",
+                        headers: {
+                                    "Authorization": API_KEY,
+                                 }
+                        })
+}
+
+async function getStatus(e) {
+
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayStatus(data);
+    } else {
+        throw new Error(data.error);
+    }
+
+}
+
+function displayStatus(data) {
+
+    let heading = "API Key Status";
+    let results = `<div>Your key is valid until</div>`;
+    results += `<div class="key-status">${data.expiry}</div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
+
+}
