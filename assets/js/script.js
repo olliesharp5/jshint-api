6fc7cfa68b59826d6661b85a605e6f8582a68414
Oliver Sharp <olliesharp5@gmail.com>
2024-01-12 13:42:15 +0000

    Added lesson content

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,42 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e));
 
-async function postForm(e) {
-    const form = new FormData(document.getElementById("checksform"));
 
-    const response = fetch("https://ci-jshint.herokuapp.com/api", {
-                        method: "POST",
-                        headers: {
-                                    "Authorization": API_KEY,
-                                 }
-                        })
+function processOptions(form) {
+    let optArray = [];
+
+    for (let e of form.entries()) {
+        if (e[0] === "options") {
+            optArray.push(e[1]);
+        }
+    }
+
+    form.delete("options");
+
+    form.append("options", optArray.join());
+
+    return form;
+}
+
+
+async function postForm(e) {
+    const form = processOptions(new FormData(document.getElementById("checksform")));
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        },
+                        body: form, 
+    });
+
+    const data = await response.json();
+    if (response.ok) {
+        displayErrors(data);
+    } else {
+        displayException(data);
+        throw new error(data.error);
+    }
 }
 
 async function getStatus(e) {
@@ -27,9 +54,31 @@ async function getStatus(e) {
     if (response.ok) {
         displayStatus(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 
+}
+
+function displayErrors(data) {
+
+    let results = "";
+
+    let heading = `JSHint Results for ${data.file}`;
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors">No errors reported!</div>`;
+    } else {
+        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`;
+        for (let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span>, `;
+            results += `column <span class="column">${error.col}:</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
+    }
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
 }
 
 function displayStatus(data) {
@@ -42,4 +91,18 @@ function displayStatus(data) {
     document.getElementById("results-content").innerHTML = results;
     resultsModal.show();
 
+}
+
+function displayException(data) {
+
+    let heading = "An Exception Occurred";
+
+    results = `<div>The API returned status code ${data.status_code}</div>`; 
+    results += `<div>Error number: <strong>${data.error_no}</strong></div>`;
+    results += `<div>Error text: <strong>${data.error}</strong></div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show();
 }
